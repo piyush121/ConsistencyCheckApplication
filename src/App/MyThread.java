94d9c290d2fd04f261079acd0b00602ad6e789e6
anikb92 <anikb92@gmail.com>
2016-12-09 15:13:06 -0500

    Added necessary dependencies.

--- src/App/MyThread.java
+++ src/App/MyThread.java
@@ -3,6 +3,15 @@
  */
 package App;
 
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * This file will act like one client. Several clients will run all together to test
  * the consistency of any distributed server( Key-value server in this case).
